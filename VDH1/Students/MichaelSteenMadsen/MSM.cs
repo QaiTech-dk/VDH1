3b4467b062dd07fe9c79807b647ba8d04a9d683d
QaiTech-dk <qaitech@mail.dk>
2021-06-03 09:11:49 +0200

    2021-06-03 - 09:11

---
+++ VDH1/Students/MichaelSteenMadsen/MSM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VDH1.Students.MichaelSteenMadsen
+{
+    class MSM
+    {
+        //  Property
+        public string FullName { get; set; }
+
+        //  Constructor
+        public MSM(string fullname)
+        {
+            this.FullName = fullname;
+        }
+
+        //  Override
+        public override string ToString()
+        {
+            return $"Michael Steen Madsen";
+        }
+    }
+}
